d8af5b52d7d53ffa3c11464d8fbb7916f638aea2
kumamihir <mihirkumar1208@gmail.com>
2024-11-29 15:01:06 +0000

    compare of datatype

---
+++ .devcontainer/01_Basics-JS/comparison.js
@@ -0,0 +1,14 @@
+// console.log(2>1)
+// console.log("1" == 1 )
+// console.log(1>=1)
+// console.log(1<1)
+
+//===
+
+console.log("1" === 1)
+
+//=== three equal also check their datatype
+
+console.log(null == 0);// output will be false
+
+console.log(undefined == 0) // output will be false;
