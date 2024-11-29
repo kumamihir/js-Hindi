0a1f0139f71d446979826b8e1d767bcdd6d4ff40
kumamihir <mihirkumar1208@gmail.com>
2024-11-29 09:36:33 +0000

    container2 config done

---
+++ .devcontainer/01_Basics-JS/variable.js
@@ -0,0 +1,21 @@
+const accountid=1534;
+let accountemail="mihir675@gmail.com";
+var accountpassword="12345";
+accountcity="jaipur";
+// accountid=2;
+// console.log(accountid);
+accountemail="hc@gmail.com";
+accountpassword="23";
+accountcity="delhi";
+let accountstate'
+// console.log("accountemail"); if you use this console.log there will be more than 3 typed console and taking much time instead of using this you can sue console.table();
+//we Know const varibale cant be changed further if it asssign any value first in the code;
+
+console.table([accountid,accountemail,accountpassword,accountcity,accountstate])
+
+/*
+
+prefer not to use var
+bacause of issue in block scope and functional scope;
+
+*/
