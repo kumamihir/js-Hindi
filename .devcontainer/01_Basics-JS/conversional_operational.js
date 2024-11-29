103ea66a55715e31fbd9fb2d095f3ff104962c4b
kumamihir <mihirkumar1208@gmail.com>
2024-11-29 14:15:41 +0000

    conversion-of-datatype

---
+++ .devcontainer/01_Basics-JS/conversional_operational.js
@@ -0,0 +1,12 @@
+// let score=33;
+// console.log(typeof score)
+ //if we do score="33" its type will be string 
+
+ let score="33"
+ console.log(typeof score)
+  // if we want to work on the number type we can convert it from string to number
+  let valueinnumber=Number(score);
+  console.log( typeof valueinnumber);
+  console.log(valueinnumber)
+
+  //and we can do same for number to string typeof if we want to work on string typeof datatype
