8b5845a61099508fa41d55965d9c66d0a46d48d1
kumamihir <mihirkumar1208@gmail.com>
2024-11-29 10:04:45 +0000

    datatype

---
+++ .devcontainer/01_Basics-JS/datatype.js
@@ -0,0 +1,30 @@
+"use strict" //treat all js code as newer version;
+
+// alert( 3 + 3);// this alert syntax will only be used in console window bcoz js engine is setup in the browser
+// for using alert in node there is an another method we will learn it soon;
+
+
+let name="Mihir";
+let age= 19;
+let loggedin=true;
+
+//name is string datatype
+//age is  a number datetype
+//loggdin is a boolean datatype;
+
+
+/// some primitive datatypes;
+
+//number => 2 to the power of 53
+//Bigint
+//string =>""
+//boolean
+//null =>standalone value
+//undefined
+//symbol =>unique
+
+
+
+// if you want to know which datatype is being used in the code you can easily do this;
+
+console.log(typeof "Mihir");
