2b62e3ddc5daea069b147812213d449b37c65d9f
kumamihir <mihirkumar1208@gmail.com>
2024-11-30 09:56:28 +0000

    dateplay

---
+++ .devcontainer/01_Basics-JS/date_time.js
@@ -0,0 +1,14 @@
+//  DATE
+
+const mydate = new Date()
+
+console.log(mydate)
+
+// but if we try to change this date to string lets see
+
+console.log(mydate.toString());
+
+// console.log(mydate.toDateString());
+// console.log(mydate.toISOString());
+// console.log(mydate.toLocaleString());
+console.log(mydate.toJSON());
