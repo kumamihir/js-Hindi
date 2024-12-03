03b431b10fd78e2731ba6a2676e80fcf87d9a313
MIHIR KUMAR <185640021+kumamihir@users.noreply.github.com>
2024-12-03 11:07:51 +0000

    onejs

---
+++ 04_control_flow/one.js
@@ -0,0 +1,22 @@
+//if statement
+
+//syntax
+
+// if (condition) {
+    
+// }
+
+
+//else syntax
+// else{
+
+// }
+
+
+const temp = 42
+if(temp>40){
+    console.log("temp is high")
+}
+else{
+    console.log("normal temp")
+}
