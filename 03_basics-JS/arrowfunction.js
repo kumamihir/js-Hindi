5f22f7a2e5fe40f5c53b7691e231024a6c108bf6
MIHIR KUMAR <185640021+kumamihir@users.noreply.github.com>
2024-12-03 06:01:21 +0000

    arrowfunc

--- 03_basics-JS/arrowfunction.js
+++ 03_basics-JS/arrowfunction.js
@@ -11,7 +11,7 @@ const user = {
 
         //what if we print just "this";
 
-        console.log(this)
+        // console.log(this)
 
         //this is the out put
 
@@ -36,3 +36,48 @@ user.welcomemessage()
 //if this time user name changed to sameer
 user.username = "sameer"
 user.welcomemessage()
+
+
+//if you do 
+
+function chai(){
+     let usernames = "mihir"
+    console.log(this.usernames) //this is not working bcoz this only works under objects
+    //in upper one functions arein objects
+
+    // if you use let before writing usernames it gives output of "undefined"
+    //but if you dont give the let variable it give output normally "mihir";
+}
+chai ()
+
+
+
+//**********************   ARROW FUNCTION   **********************//
+
+
+//this is an arrowfunction
+
+const arrowfun = () => {
+    let arc = "degree1"
+    console.log(arc) // output  is " degree 1"
+    console.log(this.arc) //output is "undefined"
+    console.log(this) //output is " {} ";empty object
+
+}
+arrowfun()
+
+
+const addtwo= (num1,num2) =>{
+    return num1 + num2
+}
+console.log(addtwo(3,5))
+
+//this above function can also be written as "impicit return "
+
+const addtwonew = (num1,num2) => num1 + num2 //the way of writting impicit return
+
+//for a good practice we always put up the paranthesis on the impicit return
+const addtwonewnew = (n1,n2) => ({username : "Mihir"}) //by giving paranthesis you cna easily return an object also 
+
+console.log(addtwonew(2,2))
+console.log(addtwonewnew())
