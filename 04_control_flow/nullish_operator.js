6fffb4f0eea25a596ffa3f404fee872d855f298d
MIHIR KUMAR <185640021+kumamihir@users.noreply.github.com>
2024-12-03 11:08:35 +0000

    nullish operators

---
+++ 04_control_flow/nullish_operator.js
@@ -0,0 +1,29 @@
+//Nulish coalescing operator  (??) : null undefined
+
+//this is made for counterig the back response if it doesnt come from database so a devloper can take a null value
+
+let val1;
+
+// val1 = 5 ?? 10
+
+// console.log(val1) //output = 5
+
+
+
+val1 = null ?? 10;
+
+console.log(val1) // output =10
+
+
+//Terniary opeator
+
+//syntax
+
+//condition ? true : false
+
+const user = "mihir"
+
+user == "mihir" ? console.log("jalwa") : console.log("No")
+
+
+
