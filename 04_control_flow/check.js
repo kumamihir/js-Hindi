c41453217b48818ffe6d45bc4a78532cf754e0db
MIHIR KUMAR <185640021+kumamihir@users.noreply.github.com>
2024-12-03 11:08:20 +0000

    check of array and objects

---
+++ 04_control_flow/check.js
@@ -0,0 +1,14 @@
+//checking of empty array and empty object in if statement
+
+const user = []
+if(user.length===0){
+    console.log("empty array")
+}
+
+//empty object
+
+
+const emptyobj = {}
+if(Object.keys(emptyobj).length === 0){
+    console.log("empty oject")
+}
