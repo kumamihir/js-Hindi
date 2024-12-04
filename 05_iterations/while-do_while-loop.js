814f6d20799ed58871a809ae5911dc68b60d4c49
MIHIR KUMAR <185640021+kumamihir@users.noreply.github.com>
2024-12-04 03:38:14 +0000

    while-dowhile

---
+++ 05_iterations/while-do_while-loop.js
@@ -0,0 +1,38 @@
+//while
+
+
+//syntax
+
+
+// while (condition) {
+    
+// }
+
+let index =0
+while (index<=10) {
+    console.log(  `value of index is ${index}`)
+    index+=1
+    
+}
+
+
+const myarray = ["mihir","sameer","mayank","nikhil"]
+
+let i=0;
+while (i<myarray.length) {
+    console.log(`value is ${myarray[i]}`)
+    i++;
+}
+
+
+//do-while loop
+
+let score =11;
+
+
+do{
+    console.log(`score is ${score}`)
+    score++
+
+}while (score<=10) 
+    
